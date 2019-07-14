5b97c05470fc8eabcbf43c5144aa54d12f4a3e04
André Pardal <andrecspardal@gmail.com>
2019-07-14 11:59:45 +0100

    React native screens example

--- android/app/src/main/java/com/reactnativeprototype/MainActivity.java
+++ android/app/src/main/java/com/reactnativeprototype/MainActivity.java
@@ -1,9 +1,13 @@
 package com.reactnativeprototype;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.facebook.react.ReactFragmentActivity;
 
-public class MainActivity extends ReactActivity {
-
+public class MainActivity extends ReactFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
